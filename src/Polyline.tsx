e439ba56336ac162405f42ca6b200f7733cdadf7
Benjythebee <Benjy.larcher@aol.com>
2021-10-02 16:37:44 +1300

    clean up of tests; Removed all tests and add tslint and typescript jest

--- src/Polyline.tsx
+++ src/Polyline.tsx
@@ -1,6 +1,6 @@
 import { Polyline as LeafletPolyline } from 'leaflet';
 import createLayer from './create-layer';
-import { LayerTypeConstructable } from './types/create-layer';
+import { LayerTypeConstructable } from './types';
 
 const Polyline = createLayer(LeafletPolyline as unknown as LayerTypeConstructable, 'positions', { componentName: 'Polyline' });
 
